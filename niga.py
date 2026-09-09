9c9187ea4580ace61c9960db0433ebc58da13b3a
hrsht911 <harshityadavpune81@gmail.com>
2026-09-09 15:00:40 +0530

    first commit

---
+++ niga.py
@@ -0,0 +1,14 @@
+s1 = {1,2,3}
+s2 = {2,3,4}
+print(s1 | s2) #union
+print(s1 & s2) #intersection
+print(s1 - s2) #difference
+print(s1 ^ s2) #symmetric difference
+print("union of s1 and s2:", s1.union(s2))
+print("intersection of s1 and s2:", s1.intersection(s2))
+print("difference of s1 and s2:", s1.difference(s2))
+print("difference of s2 and s1:", s2.difference(s1))
+print("symmetric difference of s1 and s2:", s1.symmetric_difference(s2))
+num = int(input("Enter a number: " "))
+fact = 1
+for i in range(1, num + 1):

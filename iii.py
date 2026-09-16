d36a24716179435102f32a679ba0ddc9cfd5c926
hrsht911 <harshityadavpune81@gmail.com>
2026-09-16 09:48:17 +0530

    did this in classroom

---
+++ iii.py
@@ -0,0 +1,32 @@
+#BFS program
+
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+#example graph
+
+graph= {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Run BFS starting from node 'A'
+bfs(graph, 'A')
